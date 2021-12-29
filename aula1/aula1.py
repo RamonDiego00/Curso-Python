0585d79ec54e4230328a7d168675bbddf779c4af
Ramon Diego dos Santos Ferreira <ramondiego857@gmail.com>
2021-12-29 16:46:37 -0300

    Aula comentários

---
+++ aula1/aula1.py
@@ -0,0 +1,16 @@
+print('Ramon zão 1')  # outro comentario
+print('Ramon zão 2')  # outro
+# Algum comentário sobre essa linha
+# Outra linha
+print('Ramon zão 3')
+# print('Ramon zão 3')
+print('Ramon zão 4')  # Opaaaaa
+print('Ramon zão 5')  # Não se perca amigão
+"""
+Esse é um 
+comentário de 
+multiplas linhas 
+"""
+
+#Explicação
+print('Ramon voce é muito bonito e inteligente') # Quem sou eu

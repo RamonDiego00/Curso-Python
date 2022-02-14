45f5820ece66f50d567e53343fd4b73982232b8c
Ramon Diego dos Santos Ferreira <ramondiego857@gmail.com>
2022-02-14 20:36:15 -0300

    Indices e while

---
+++ manipulandoStrings/IndicesStrings.py
@@ -0,0 +1,16 @@
+#positivos [01234567]
+texto = 'Python_s2'
+#negativos -[98765432]
+
+print(texto[4])
+
+url = 'www.google.com.br/'
+#retirando ultimo caractere
+print(url[:-1])
+
+# nova_string = texto[2:6]
+nova_string = texto[0::2]
+#fatiamento
+print(nova_string)
+
+print(len(texto))
